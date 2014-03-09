43e02f5ab5fc4900b57ca319e470fbd17ec55ce4
Bradley J. Spaulding <bspaulding@dataxu.com>
2014-03-09 15:10:34 -0400

    Added npm config & grunt w/ jshint

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        'lib/{,*/}*.js',
+        'test/spec/{,*/}*.js'
+      ]
+    }
+  });
+
+  grunt.registerTask('default', [
+    'newer:jshint'
+  ]);
+};
